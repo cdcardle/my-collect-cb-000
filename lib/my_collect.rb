3b22d689b7f420f914be3dc0197bbe2a2072cee0
Chris Cardle <cdcardle@gmail.com>
2017-10-30 15:29:40 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
     i = 0
     collections = []
     while i < array.length
-      collections >> yield(array[i])
+      collections << yield(array[i])
     end
     collections
   end
